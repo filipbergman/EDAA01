320cc140cf5925030c214d50af7698fd0721f049
kr6356zo-s <kr6356zo-s@falk-9.e.lth.lu.se>
2018-11-13 14:45:50 +0100

    Board grundmetoder

--- SudokuSolver/src/Board.java
+++ SudokuSolver/src/Board.java
@@ -1,4 +1,35 @@
 
 public class Board {
-
+	private int board[][] = new int[9][9];
+	
+	
+	public boolean set (int row, int col, int number) {
+		if (row < 10 && row > 0 && col < 10 && col > 0 && number < 10 && number > 0) {
+			board[row][col] = number;
+			return true;
+		}
+		return false;
+	}
+	public int getNum(int row, int col) {
+		if (row < 10 && row > 0 && col < 10 && col > 0) {
+			return board[row][col];
+		}
+		return -1;
+	}
+	public int remove (int row, int col) {
+		int temp = board[row][col];
+		board[row][col] = 0;
+		return temp;
+	}
+	public void clear (int row, int col) {
+		board = new int[9][9];
+	}
+	
+	public boolean solve() {
+		return false;
+	}
+	
+	private boolean solve(int i, int j) {
+		return false;
+	}
 }
