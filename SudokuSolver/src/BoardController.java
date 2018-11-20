105e231b476d57661fcc1ae48b9fb65cd9661bc0
Filip B <Filip B@LAPTOP-RU50QM88>
2018-11-20 16:00:38 +0100

    -

--- SudokuSolver/src/BoardController.java
+++ SudokuSolver/src/BoardController.java
@@ -1,4 +1,31 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
 
-public class BoardController {
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionModel;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.stage.Stage;
+
+
+public class BoardController extends Application {
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+	}
 
 }
