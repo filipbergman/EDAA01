938c6f62a4394041c21a85675747ab88b2c674ce
kr6356zo-s <kr6356zo-s@panter-9.e.lth.lu.se>
2018-11-20 15:25:15 +0100

    Can now solve a sudoku board. Added tests too.

--- SudokuSolver/src/Board.java
+++ SudokuSolver/src/Board.java
@@ -3,7 +3,7 @@ public class Board {
 	private int board[][] = new int[9][9];
 
 	public boolean set(int row, int col, int number) {
-		if (row < 9 && row >= 0 && col < 9 && col >= 0 && number < 9 && number > 0 && board[row][col] != 0) {
+		if (row < 9 && row >= 0 && col < 9 && col >= 0 && number < 10 && number > 0 && board[row][col] == 0) {
 			board[row][col] = number;
 			return true;
 		}
@@ -11,7 +11,7 @@ public class Board {
 	}
 
 	public int getNum(int row, int col) {
-		if (row < 10 && row > 0 && col < 10 && col > 0) {
+		if (row < 9 && row >= 0 && col < 9 && col >= 0) {
 			return board[row][col];
 		}
 		return -1;
@@ -38,39 +38,59 @@ public class Board {
 
 		for (int num = 1; num < 10; num++) {
 			if (col == 9) {
-				solve(row + 1, 0);
-			}
-			set(row, col, num);
-			//Möjligtvis måste detta ändras då vi ska kolla ruleCheck efter varje set.
-			if (!solve(row, col + 1)) {
-				remove(row, col);
-			}
-			else return true;
+				if (solve(row + 1, 0))
+					return true;
+			} else {
+				boolean numbSet = set(row, col, num);
 
+				if (ruleCheck(row, col, getNum(row, col))) {
+					if (!solve(row, col + 1)) {
+						if (!numbSet)
+							break;
+						remove(row, col);
+					} else
+						return true;
+				} else if (numbSet) remove(row, col);
+			}
 		}
 
 		return false;
 	}
-	
+
 	private boolean ruleCheck(int row, int col, int number) {
 		for (int c = 0; c < 9; c++) {
-			if (getNum(row,c) == number && c != col) return false;
+			if (getNum(row, c) == number && c != col)
+				return false;
 		}
 		for (int r = 0; r < 9; r++) {
-			if (getNum(r,col) == number && r != row) return false;
+			if (getNum(r, col) == number && r != row)
+				return false;
 		}
-		
+
 		int rMin = row - row % 3;
 		int rMax = rMin + 3;
-		
+
 		int cMin = col - col % 3;
 		int cMax = cMin + 3;
-		
+
 		for (int r = rMin; r < rMax; r++) {
 			for (int c = cMin; c < cMax; c++) {
-				if (getNum(r,c) == number && (c != col && r != row)) return false;
+				if (getNum(r, c) == number && (c != col && r != row))
+					return false;
 			}
 		}
 		return true;
+	}
+	
+	@Override
+	public String toString() {
+		String string = "";
+		for(int row = 0; row < 9; row++) {
+			for(int col = 0; col < 9; col++) {
+				string += getNum(row,col) + " ";
+			}
+			string += "\n";
+		}
+		return string;
 	}
 }
---
+++ SudokuSolver/src/BoardTest.java
@@ -0,0 +1,87 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BoardTest {
+	Board board;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		board = new Board();
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		
+	}
+	
+	@Test
+	void testGetNum() {
+		board.set(1, 3, 9);
+		assertEquals(9, board.getNum(1, 3));
+	}
+	
+	@Test
+	void testSetonBusy() {
+		board.set(1, 3, 9);
+		assertTrue(!board.set(1, 3, 9));
+	}
+
+	@Test
+	void testEmpty() {
+		assertTrue(board.solve());
+		assertEquals(9, board.getNum(0,8));
+	}
+	
+	@Test
+	void testUnsolvable() {
+		board.set(0, 2, 8);
+		board.set(1, 1, 8);
+		assertFalse(board.solve());
+	}
+	
+	@Test
+	void testFig1() {
+		board.set(0, 2, 8);
+		board.set(0, 5, 9);
+		board.set(0, 7, 6);
+		board.set(0, 8, 2);
+		
+		board.set(1, 8, 5);
+		
+		board.set(2, 0, 1);
+		board.set(2, 2, 2);
+		board.set(2, 3, 5);
+		
+		board.set(3, 3, 2);
+		board.set(3, 4, 1);
+		board.set(3, 7, 9);
+		
+		board.set(4, 1, 5);
+		board.set(4, 6, 6);
+		
+		board.set(5, 0, 6);
+		board.set(5, 7, 2);
+		board.set(5, 8, 8);
+		
+		board.set(6, 0, 4);
+		board.set(6, 1, 1);
+		board.set(6, 3, 6);
+		board.set(6, 5, 8);
+		
+		board.set(7, 0, 8);
+		board.set(7, 1, 6);
+		board.set(7, 4, 3);
+		board.set(7, 6, 1);
+		
+		board.set(8, 6, 4);
+		
+		assertTrue(board.solve());
+		System.out.println(board);
+		
+		//assertEquals(9, board.getNum(0,8));
+	}
+
+}
