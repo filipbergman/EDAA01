a0b955502c7bf61f7d5e80912ede9992fe29fbaf
Filip B <Filip B@LAPTOP-RU50QM88>
2018-11-27 16:34:19 +0100

    Fixed the graphic and alignement of the tiles

--- SudokuSolver/src/BoardController.java
+++ SudokuSolver/src/BoardController.java
@@ -12,28 +12,33 @@ import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
 
 public class BoardController extends Application {
-	TilePane tile = new TilePane();
+	
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		TilePane tile = new TilePane();
 		Board board = new Board();
 		BorderPane root = new BorderPane();
-		root.getChildren().addAll(tile);
+		HBox hbox1 = new HBox();
+		root.setCenter(hbox1);
+		hbox1.setAlignment(Pos.CENTER);
+		hbox1.getChildren().addAll(tile);
 		
-		Scene scene = new Scene(root, 700, 600);
+		
+		Scene scene = new Scene(root, 400, 350);
 		primaryStage.setTitle("Sudoku");
 		primaryStage.setScene(scene);
 		primaryStage.show();
 
-		HBox hbox = new HBox();
+		HBox hbox2 = new HBox();
 		Button solve = new Button("Solve");
 		Button clear = new Button("Clear");
 
 		solve.setDefaultButton(true);
 
-		hbox.getChildren().addAll(solve, clear);
-		root.setBottom(hbox);
-//		root.setCenter(tile);
+		hbox2.getChildren().addAll(solve, clear);
+		root.setBottom(hbox2);
+		
 		solve.setOnAction(event -> {
 			calculateBoard();
 
@@ -45,23 +50,28 @@ public class BoardController extends Application {
 		tile.setHgap(8);
 		ArrayList<TilePane> sections = new ArrayList<TilePane>();
 		tile.setPrefColumns(3);
+		tile.setHgap(2);
+		tile.setVgap(2);
+		
 		
 		
 		for (int i = 0; i < 9; i++) {
 			ArrayList<OneNumberTextField> textFields = new ArrayList<OneNumberTextField>();
-			sections.add(new TilePane());
-			
-			// Ändra färger varannan tilepane
-			if (i % 2 == 0) {
-
-			}
+			TilePane t = new TilePane();
+			sections.add(t);
+			t.setPrefColumns(3);
 
 			tile.getChildren().addAll(sections.get(i));
 			for (int k = 0; k < 9; k++) {
 				textFields.add(new OneNumberTextField());
-				textFields.get(k).setPrefHeight(1);
-				textFields.get(k).setPrefWidth(1);
-
+				textFields.get(k).setPrefHeight(30);
+				textFields.get(k).setPrefWidth(30);
+				// Ändra färger varannan tilepane
+				if (i % 2 == 0) {
+					textFields.get(k).setStyle("-fx-background-color: red;");
+				} else 
+					textFields.get(k).setStyle("-fx-background-color: blue;");
+				
 				sections.get(i).getChildren().addAll(textFields.get(k));
 			}
 		}
--- lab5/src/bst/BinarySearchTree.java
+++ lab5/src/bst/BinarySearchTree.java
@@ -7,14 +7,18 @@ public class BinarySearchTree<E extends Comparable<? super E>> {
 	public static void main(String args[]) {
 		BinarySearchTree<Integer> tree = new BinarySearchTree<Integer>();
 		BSTVisualizer bstv = new BSTVisualizer("Träed", 500, 500);
-		tree.add(7);
-		tree.add(6);
-		tree.add(5);
-		tree.add(4);
-		tree.add(3);
+//		tree.add(7);
+//		tree.add(6);
+//		tree.add(5);
+//		tree.add(4);
+//		tree.add(3);
+//		tree.add(2);
+//		tree.add(1);
+		tree.add(1);
 		tree.add(2);
-		tree.add(1);
-		tree.add(1);
+		tree.add(3);
+		tree.add(4);
+		tree.add(5);
 		tree.rebuild();
 		bstv.drawTree(tree);
 		tree.printTree();
